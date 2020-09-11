ffe5cebd7dc61a9c8fefbc785f69365011ced7d9
p_pbluewang <p_pbluewang@tencent.com>
2020-09-11 18:30:40 +0800

    首页

--- taro/src/pages/index/index.jsx
+++ taro/src/pages/index/index.jsx
@@ -1,48 +1,44 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { View, Button, Text } from '@tarojs/components'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { View, Button, Text } from "@tarojs/components";
 
-import { add, minus, asyncAdd } from '../../actions/counter'
-
-import './index.scss'
-
+import "./index.scss";
 
 @connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
+  counter,
 }))
 class Index extends Component {
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps);
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
     return (
-      <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
+      <View className="index">
+        <Button className="add_btn" onClick={this.props.add}>
+          +
+        </Button>
+        <Button className="dec_btn" onClick={this.props.dec}>
+          -
+        </Button>
+        <Button className="dec_btn" onClick={this.props.asyncAdd}>
+          async
+        </Button>
+        <View>
+          <Text>{this.props.counter.num}</Text>
+        </View>
+        <View>
+          <Text>Hello, World</Text>
+        </View>
       </View>
-    )
+    );
   }
 }
 
-export default Index
-
+export default Index;
